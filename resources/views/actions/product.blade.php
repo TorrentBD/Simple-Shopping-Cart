12dc9cd71d03613d067b5b40b770fba1e4ca7757
Mituram <mituramray09@gmail.com>
2018-03-17 00:47:46 +0600

    first commit

---
+++ resources/views/actions/product.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('title','Product view')
+
+@section('content')
+    <div class="row">
+        <div class="container">
+            <h3>Product List </h3>
+        </div>
+    </div>
+ 
+@endsection
